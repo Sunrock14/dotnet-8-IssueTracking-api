084e9e3780b91963224d7580e37d2144d9e4f691
Sunrock14 <farukkocademir@hotmail.com>
2024-06-29 19:11:15 +0300

    Unit of work

---
+++ IssueTracking.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using IssueTracking.Data.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracking.Data.UnitOfWork;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    ICommentRepository CommentRepository { get; }
+    IFileAttachmentRepository FileAttachmentRepository{ get; }
+    IIssueAssigneeRepository IssueAssigneeRepository { get; }
+    IIssueRepository IssueRepository  { get; }
+    IProjectRepository ProjectRepository  { get; }
+    IPriorityRepository PriorityRepository  { get; }
+    IRoleRepository RoleRepository  { get; }
+    IStatusRepository StatusRepository  { get; }
+    ITimeEntryRepository TimeEntryRepository  { get; }
+    IUserRepository UserRepository  { get; }
+
+    Task<int> SaveAsync();
+
+}
---
+++ IssueTracking.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using IssueTracking.Data.Repositories.Abstract;
+using IssueTracking.Data.Repositories.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracking.Data.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+    private CommentRepository _commentRepository;
+    private FileAttachmentRepository _fileAttachmentRepository;
+    private IssueAssigneRepository  _ıssueAssigneRepository;
+    private IssueRepository _ıssueRepository;
+    private PriorityRepository _priorityRepository;
+    private ProjectRepository _projectRepository;
+    private RoleRepository _roleRepository;
+    private StatusRepository _statusRepository;
+    private TimeEntryRepository _timeEntryRepository;
+    private UserRepository _userRepository;
+
+    public UnitOfWork(DbContext context)
+    {
+        _context = context;
+    }
+
+    public ICommentRepository CommentRepository => _commentRepository ??= new CommentRepository(_context);
+
+    public IFileAttachmentRepository FileAttachmentRepository => _fileAttachmentRepository ??= new FileAttachmentRepository(_context);
+
+    public IIssueAssigneeRepository IssueAssigneeRepository => _ıssueAssigneRepository ??= new IssueAssigneRepository(_context);
+
+    public IIssueRepository IssueRepository => _ıssueRepository ??= new IssueRepository(_context);
+
+    public IProjectRepository ProjectRepository => _projectRepository ??= new ProjectRepository(_context);
+
+    public IPriorityRepository PriorityRepository => _priorityRepository??= new PriorityRepository(_context);
+
+    public IRoleRepository RoleRepository => _roleRepository ??= new RoleRepository(_context);
+
+    public IStatusRepository StatusRepository => _statusRepository??= new StatusRepository(_context);
+
+    public ITimeEntryRepository TimeEntryRepository => _timeEntryRepository??= new TimeEntryRepository(_context);
+
+    public IUserRepository UserRepository => _userRepository ??= new UserRepository(_context);
+
+    public async Task<int> SaveAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _context.DisposeAsync();
+    }
+}
